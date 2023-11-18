5553e273822f5e9ce7ac11fa0da6645c287981b7
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-11-18 20:46:37 +0800

    fix some bug and format the code (#4)
    * little change
    
    * create a gitignore file
    
    * little change
    
    * fix some bug and format the code

--- pages/detail-message/detail-message.js
+++ pages/detail-message/detail-message.js
@@ -1,164 +1,157 @@
 // pages/all-message/all-message.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     menutop: 0,
-    menuright: '',
+    menuright: "",
     menuheight: 0,
     statusheight: 0,
     allmessage: [],
     contentheight: [],
-    id: '',
+    id: "",
     page: 0,
     size: 5,
-    number: ''
+    number: "",
   },
-  before(){
-    wx.navigateBack()
+  before() {
+    wx.navigateBack();
   },
-  enlarge(e){
+  enlarge(e) {
     wx.previewImage({
       current: e.currentTarget.dataset.src,
-      urls: [...e.currentTarget.dataset.id]
-    })
+      urls: [...e.currentTarget.dataset.id],
+    });
   },
-  addlove(){
+  addlove() {
     wx.showToast({
-      title: '仅可在动态页面点赞哦！',
-      icon: 'none'
-    })
-},
-todongtai(e){
-  wx.navigateTo({
-    url: `/pages/comment/comment?id=${e.currentTarget.dataset.id}`
-  })
-},
+      title: "仅可在动态页面点赞哦！",
+      icon: "none",
+    });
+  },
+  todongtai(e) {
+    wx.navigateTo({
+      url: `/pages/comment/comment?id=${e.currentTarget.dataset.id}`,
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
     this.setData({
-      number: options.number
-    })
+      number: options.number,
+    });
     wx.request({
-      url: 'https://yellowwei.cn:443/publish/five',
-      method: 'GET',
+      url: "https://yellowwei.cn:443/publish/five",
+      method: "GET",
       header: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data: {
         number: this.data.number,
         page: this.data.page * this.data.size,
-        size: this.data.size
+        size: this.data.size,
       },
-      success: res => {
-        this.data.page++
+      success: (res) => {
+        this.data.page++;
         this.setData({
-          allmessage: res.data
-        })
-      }
-    })
-    const resone = wx.getMenuButtonBoundingClientRect()
+          allmessage: res.data,
+        });
+      },
+    });
+    const resone = wx.getMenuButtonBoundingClientRect();
     wx.getSystemInfo({
-      success: restwo => {
+      success: (restwo) => {
         this.setData({
-          menuheight: resone.height * 2 ,
+          menuheight: resone.height * 2,
           menutop: resone.top * 2 + 6,
-          menuright: (restwo.windowWidth - resone.right) * 2 + 'rpx' ,
-          statusheight: restwo.statusBarHeight * 2 
-        })
-      }
-    })
+          menuright: (restwo.windowWidth - resone.right) * 2 + "rpx",
+          statusheight: restwo.statusBarHeight * 2,
+        });
+      },
+    });
     setTimeout(() => {
       let query = wx.createSelectorQuery();
-      query.selectAll('.content').boundingClientRect(rect=>{
-        this.setData({
-          contentheight: rect
+      query
+        .selectAll(".content")
+        .boundingClientRect((rect) => {
+          this.setData({
+            contentheight: rect,
+          });
         })
-        }).exec();
-      }, 500)
+        .exec();
+    }, 500);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
     wx.request({
-      url: 'https://yellowwei.cn:443/publish/five',
-      method: 'GET',
+      url: "https://yellowwei.cn:443/publish/five",
+      method: "GET",
       header: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data: {
         number: this.data.number,
         page: this.data.page * this.data.size,
-        size: this.data.size
+        size: this.data.size,
       },
-      success: res => {
-        if(res.data.length === 0){
+      success: (res) => {
+        if (res.data.length === 0) {
           wx.showToast({
-            title: '没有更多了~',
-            icon: 'none'
-          })
+            title: "没有更多了~",
+            icon: "none",
+          });
         }
-        this.data.page++
+        this.data.page++;
         this.setData({
-          allmessage: [...this.data.allmessage, ...res.data]
-        })
-      }
-    })
+          allmessage: [...this.data.allmessage, ...res.data],
+        });
+      },
+    });
     setTimeout(() => {
       let query = wx.createSelectorQuery();
-      query.selectAll('.content').boundingClientRect(rect=>{
-        this.setData({
-          contentheight: rect
+      query
+        .selectAll(".content")
+        .boundingClientRect((rect) => {
+          this.setData({
+            contentheight: rect,
+          });
         })
-        }).exec();
-      }, 500)
+        .exec();
+    }, 500);
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
--- pages/persondetail/persondetail.js
+++ pages/persondetail/persondetail.js
@@ -1,173 +1,175 @@
 // pages/persondetail/persondetail.js
-var get = require('../../utils/xingzuo.js')
+var get = require("../../utils/xingzuo.js");
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     menutop: 0,
-    menuright: '',
+    menuright: "",
     menuheight: 0,
     statusheight: 0,
-    nickname: '',
+    nickname: "",
     picture: [],
-    sex: '',
-    school: '',
-    grade: '',
-    hometown: '',
-    birthday: '',
-    height: '',
+    sex: "",
+    school: "",
+    grade: "",
+    hometown: "",
+    birthday: "",
+    height: "",
     year: 0,
-    month: '',
-    day: '',
+    month: "",
+    day: "",
     currentyear: 0,
-    xingzuo: '',
-    sheng: '',
-    shi: '',
+    xingzuo: "",
+    sheng: "",
+    shi: "",
     onemessage: {},
-    contentheight: 0
+    contentheight: 0,
   },
-  before(){
-      this.data.birthday.substring
-      // wx.navigateBack()
-      wx.switchTab({
-        url: '/pages/me/me',
-      })
+  before() {
+    this.data.birthday.substring;
+    // wx.navigateBack()
+    wx.switchTab({
+      url: "/pages/me/me",
+    });
   },
-  enlarge(e){
+  enlarge(e) {
     wx.previewImage({
       current: e.currentTarget.dataset.src,
       urls: [...e.currentTarget.dataset.id],
-    })
+    });
   },
-  toallmessage(e){
+  toallmessage(e) {
+    wx.navigateTo({
+      url: `/pages/all-message/all-message?number=${e.currentTarget.dataset.number}`,
+    });
+  },
+  publish() {
+    if (wx.getStorageSync("status") === "审核成功") {
       wx.navigateTo({
-        url: `/pages/all-message/all-message?number=${e.currentTarget.dataset.number}`,
-      })
+        url: "/pages/publish/publish",
+      });
+    }
+    if (wx.getStorageSync("status") === "审核失败") {
+      wx.showToast({
+        title: "资料审核未通过！",
+        icon: "none",
+      });
+    }
+    if (wx.getStorageSync("status") === "待审核") {
+      wx.showToast({
+        title: "资料审核中，请稍等一会哦！",
+        icon: "none",
+      });
+    }
   },
-  publish(){
-    wx.navigateTo({
-      url: '/pages/publish/publish',
-    })
-  },
-  addlove(){
+  addlove() {
     wx.showToast({
-      title: '仅可在动态界面点赞哦 ！',
-      icon: 'none'
-    })
+      title: "仅可在动态界面点赞哦 ！",
+      icon: "none",
+    });
   },
-  todongtai(e){
+  todongtai(e) {
     wx.navigateTo({
-      url: `/pages/comment/comment?id=${e.currentTarget.dataset.id}`
-    })
+      url: `/pages/comment/comment?id=${e.currentTarget.dataset.id}`,
+    });
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
     wx.request({
-      url: 'https://yellowwei.cn:443/publish/two',
-      method: 'GET',
+      url: "https://yellowwei.cn:443/publish/two",
+      method: "GET",
       header: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data: {
-        number: wx.getStorageSync('number')
+        number: wx.getStorageSync("number"),
       },
-      success: res => {
+      success: (res) => {
         this.setData({
-          onemessage: res.data
-        })
-      }
-    })
-    const resone = wx.getMenuButtonBoundingClientRect()
+          onemessage: res.data,
+        });
+      },
+    });
+    const resone = wx.getMenuButtonBoundingClientRect();
     wx.getSystemInfo({
-      success: restwo => {
+      success: (restwo) => {
         this.setData({
-          menuheight: resone.height * 2 ,
+          menuheight: resone.height * 2,
           menutop: resone.top * 2 + 6,
-          menuright: (restwo.windowWidth - resone.right) * 2 + 'rpx' ,
-          statusheight: restwo.statusBarHeight * 2 ,
-          nickname: wx.getStorageSync('nickname'),
-          picture: wx.getStorageSync('picture'),
-          sex: wx.getStorageSync('sex'),
-          school: wx.getStorageSync('school'),
-          grade: wx.getStorageSync('grade'),
-          hometown: wx.getStorageSync('hometown'),
-          birthday: wx.getStorageSync('birthday'),
-          height: wx.getStorageSync('height'),
-        })
-      }
-    })
+          menuright: (restwo.windowWidth - resone.right) * 2 + "rpx",
+          statusheight: restwo.statusBarHeight * 2,
+          nickname: wx.getStorageSync("nickname"),
+          picture: wx.getStorageSync("picture"),
+          sex: wx.getStorageSync("sex"),
+          school: wx.getStorageSync("school"),
+          grade: wx.getStorageSync("grade"),
+          hometown: wx.getStorageSync("hometown"),
+          birthday: wx.getStorageSync("birthday"),
+          height: wx.getStorageSync("height"),
+        });
+      },
+    });
     this.setData({
-      year: parseInt(this.data.birthday.substring(0,4)),
-      month: parseInt(this.data.birthday.substring(5,7)),
-      day: parseInt(this.data.birthday.substring(8,9)),
+      year: parseInt(this.data.birthday.substring(0, 4)),
+      month: parseInt(this.data.birthday.substring(5, 7)),
+      day: parseInt(this.data.birthday.substring(8, 9)),
       currentyear: new Date().getFullYear(),
-      sheng: this.data.hometown.substring(0,2),
-      shi: this.data.hometown.substring(3,5)
-    })
+      sheng: this.data.hometown.substring(0, 2),
+      shi: this.data.hometown.substring(3, 5),
+    });
     this.setData({
-      xingzuo: get.getxingzuo(this.data.month,this.data.day)
-    })
+      xingzuo: get.getxingzuo(this.data.month, this.data.day),
+    });
     setTimeout(() => {
       let query = wx.createSelectorQuery();
-      query.select('.content').boundingClientRect(rect=>{
-        if(rect.height){
-          this.setData({
-            contentheight: rect.height
-          })
-        }
-        }).exec();
-      }, 500)
+      query
+        .select(".content")
+        .boundingClientRect((rect) => {
+          if (rect.height) {
+            this.setData({
+              contentheight: rect.height,
+            });
+          }
+        })
+        .exec();
+    }, 500);
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-   
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
