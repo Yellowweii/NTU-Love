7021e5da48191279f86fccca43f564802d8cfeae
huang wei <1119765689hw@gmail.com>
2023-11-18 20:45:24 +0800

    fix some bug and format the code

--- component/birthday/birthday.js
+++ component/birthday/birthday.js
@@ -4,91 +4,89 @@ Component({
    * 组件的属性列表
    */
   options: {
-    styleIsolation: 'apply-shared'
+    styleIsolation: "apply-shared",
   },
-  properties: {
-
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-      flag: false,
-      year: '2002',
-      year1: '',
-      month: '11',
-      month1:'',
-      day: '04',
-      day1: '',
-      keyheight: 0
+    flag: false,
+    year: "2002",
+    year1: "",
+    month: "11",
+    month1: "",
+    day: "04",
+    day1: "",
+    keyheight: 0,
   },
-  
+
   /**
    * 组件的方法列表
    */
   methods: {
-    focusone(e){
+    focusone(e) {
+      this.setData({
+        year: "",
+        keyheight: e.detail.height,
+      });
+      this.triggerEvent("height", { value: this.data.keyheight });
+    },
+    focustwo(e) {
+      this.setData({
+        month: "",
+        keyheight: e.detail.height,
+      });
+      this.triggerEvent("height", { value: this.data.keyheight });
+    },
+    focusthree(e) {
+      this.setData({
+        day: "",
+        keyheight: e.detail.height,
+      });
+      this.triggerEvent("height", { value: this.data.keyheight });
+    },
+    inputone(e) {
+      this.setData({
+        year1: e.detail.value,
+      });
+      this.triggerEvent("year", { value: this.data.year1 });
+      if (e.detail.value.length === 4) {
         this.setData({
-          year: '',
-          keyheight: e.detail.height
-        })
-        this.triggerEvent('height',{value: this.data.keyheight})
+          flag: true,
+        });
+      }
     },
-    focustwo(e){
+    inputtwo(e) {
       this.setData({
-        month: '',
-        keyheight: e.detail.height
-      })
-      this.triggerEvent('height',{value: this.data.keyheight})
+        month1: e.detail.value,
+      });
+      this.triggerEvent("month", { value: this.data.month1 });
+    },
+    inputthree(e) {
+      this.setData({
+        day1: e.detail.value,
+      });
+      this.triggerEvent("day", { value: this.data.day1 });
+    },
+    blurone() {
+      this.setData({
+        year: "2002",
+      });
+      this.triggerEvent("height", { value: 0 });
+    },
+    blurtwo() {
+      this.setData({
+        month: "11",
+      });
+      this.triggerEvent("height", { value: 0 });
+    },
+    blurthree() {
+      this.setData({
+        day: "04",
+      });
+      this.triggerEvent("height", { value: 0 });
+    },
   },
-  focusthree(e){
-    this.setData({
-      day: '',
-      keyheight: e.detail.height
-    })
-    this.triggerEvent('height',{value: this.data.keyheight})
-  },
-    inputone(e){
-        this.setData({
-          year1: e.detail.value
-        })
-        this.triggerEvent('year',{value: this.data.year1})
-        if(e.detail.value.length === 4){
-          this.setData({
-            flag: true
-          })
-        }
-    },
-    inputtwo(e){
-        this.setData({
-          month1: e.detail.value
-        })
-        this.triggerEvent('month',{value: this.data.month1})
-    },
-    inputthree(e){
-      this.setData({
-        day1: e.detail.value
-      })
-      this.triggerEvent('day',{value: this.data.day1})
-  },
-    blurone(){
-      this.setData({
-        year:'2002'
-      })
-      this.triggerEvent('height',{value: 0})
-    },
-    blurtwo(){
-      this.setData({
-        month:'11'
-      })
-      this.triggerEvent('height',{value: 0})
-    },
-    blurthree(){
-      this.setData({
-        day:'04'
-      })
-      this.triggerEvent('height',{value: 0})
-    },
-  }
-})
+});
--- pages/all-message/all-message.js
+++ pages/all-message/all-message.js
@@ -1,205 +1,198 @@
 // pages/all-message/all-message.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     menutop: 0,
-    menuright: '',
+    menuright: "",
     menuheight: 0,
     statusheight: 0,
     allmessage: [],
     contentheight: [],
     show: false,
-    id: '',
+    id: "",
     page: 0,
     size: 5,
-    number: '',
+    number: "",
     switch: 0,
-    j: 0
+    j: 0,
   },
-  before(){
-    wx.navigateBack()
+  before() {
+    wx.navigateBack();
   },
-  close(e){
+  close(e) {
     this.setData({
       show: !this.data.show,
-      id: e.currentTarget.dataset.id
-    })
+      id: e.currentTarget.dataset.id,
+    });
   },
-  quxiao(){
+  quxiao() {
     this.setData({
-      show: false
-    })
+      show: false,
+    });
   },
-  delete(){
+  delete() {
     wx.request({
-      url: 'https://yellowwei.cn:443/publish',
-      method: 'DELETE',
+      url: "https://yellowwei.cn:443/publish",
+      method: "DELETE",
       header: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data: {
-        id: this.data.id
+        id: this.data.id,
       },
-      success: res => {
-        this.onLoad()
+      success: (res) => {
+        this.onLoad();
         this.setData({
-          show: false
-        })
+          show: false,
+        });
         wx.showToast({
-          title: '删除成功',
-          icon: 'none'
-        })
-      }
-    })
+          title: "删除成功",
+          icon: "none",
+        });
+      },
+    });
   },
-  enlarge(e){
+  enlarge(e) {
     wx.previewImage({
       current: e.currentTarget.dataset.src,
-      urls: [...e.currentTarget.dataset.id]
-    })
+      urls: [...e.currentTarget.dataset.id],
+    });
   },
-  addlove(e){
+  addlove(e) {
     wx.showToast({
-      title: '仅可在动态界面点赞哦 ！',
-      icon: 'none'
-    })
+      title: "仅可在动态界面点赞哦 ！",
+      icon: "none",
+    });
   },
-  todongtai(e){
+  todongtai(e) {
     wx.navigateTo({
-      url: `/pages/comment/comment?id=${e.currentTarget.dataset.id}`
-    })
+      url: `/pages/comment/comment?id=${e.currentTarget.dataset.id}`,
+    });
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-      this.setData({
-        number: options.number
-      })
+    this.setData({
+      number: options.number,
+    });
     wx.request({
-      url: 'https://yellowwei.cn:443/publish/three',
-      method: 'GET',
+      url: "https://yellowwei.cn:443/publish/three",
+      method: "GET",
       header: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data: {
         number: this.data.number,
         page: this.data.page * this.data.size,
-        size: this.data.size
+        size: this.data.size,
       },
-      success: res => {
+      success: (res) => {
         this.setData({
-          allmessage: res.data
-        })
-      }
-    })
-    const resone = wx.getMenuButtonBoundingClientRect()
+          allmessage: res.data,
+        });
+      },
+    });
+    const resone = wx.getMenuButtonBoundingClientRect();
     wx.getSystemInfo({
-      success: restwo => {
+      success: (restwo) => {
         this.setData({
-          menuheight: resone.height * 2 ,
+          menuheight: resone.height * 2,
           menutop: resone.top * 2 + 6,
-          menuright: (restwo.windowWidth - resone.right) * 2 + 'rpx' ,
-          statusheight: restwo.statusBarHeight * 2 
-        })
-      }
-    })
+          menuright: (restwo.windowWidth - resone.right) * 2 + "rpx",
+          statusheight: restwo.statusBarHeight * 2,
+        });
+      },
+    });
     setTimeout(() => {
       let query = wx.createSelectorQuery();
-      query.selectAll('.content').boundingClientRect(rect=>{
-        this.setData({
-          contentheight: rect
+      query
+        .selectAll(".content")
+        .boundingClientRect((rect) => {
+          this.setData({
+            contentheight: rect,
+          });
         })
-        }).exec();
-    }, 500)
+        .exec();
+    }, 500);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-     
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-      
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-if(this.data.switch === 0){
-    this.data.page++
-    wx.request({
-      url: 'https://yellowwei.cn:443/publish/three',
-      method: 'GET',
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      data: {
-        number: this.data.number,
-        page: this.data.page * this.data.size,
-        size: this.data.size
-      },
-      success: res => {
-        if(res.data.length === 0){
-          this.data.page--
+    if (this.data.switch === 0) {
+      this.data.page++;
+      wx.request({
+        url: "https://yellowwei.cn:443/publish/three",
+        method: "GET",
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: {
+          number: this.data.number,
+          page: this.data.page * this.data.size,
+          size: this.data.size,
+        },
+        success: (res) => {
+          if (res.data.length === 0) {
+            this.data.page--;
+            this.setData({
+              switch: 1,
+            });
+            wx.showToast({
+              title: "没有更多了~",
+              icon: "none",
+            });
+          }
           this.setData({
-            switch: 1
+            allmessage: [...this.data.allmessage, ...res.data],
+          });
+        },
+      });
+      setTimeout(() => {
+        let query = wx.createSelectorQuery();
+        query
+          .selectAll(".content")
+          .boundingClientRect((rect) => {
+            this.setData({
+              contentheight: rect,
+            });
           })
-          wx.showToast({
-            title: '没有更多了~',
-            icon: 'none'
-          })
-        }
-        this.setData({
-          allmessage: [...this.data.allmessage, ...res.data]
-        })
-      }
-    })
-    setTimeout(() => {
-      let query = wx.createSelectorQuery();
-      query.selectAll('.content').boundingClientRect(rect=>{
-        this.setData({
-          contentheight: rect
-        })
-        }).exec();
-    }, 500)
+          .exec();
+      }, 500);
     }
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
